bb16fc00fd818aa588afdb5a0b9434213c0ceee4
Brendan Darnell <brendan_darnell@hotmail.com>
2019-02-22 05:17:51 -0700

    Add color scheme to page and Lemme Know app content

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function toggleSidebar() {
 	$('.toggle-button').on('click', (e) => {
 		e.stopPropagation;
 		$('.sidebar').toggleClass('sidebar-hidden');
+		if($('.sidebar').hasClass('sidebar-hidden')) {
+			$('.toggle-button').text('>');
+		}
+		else {
+			$('.toggle-button').text('<');
+		}
 	})
 };
 
